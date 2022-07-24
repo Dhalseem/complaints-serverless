cc6efc1588dba78076a77a1f97942629ef8b03a9
avinash.jayakar <avithemad@gmail.com>
2022-07-24 13:01:59 +0530

    Added initial google sheets api to the function

--- src/api.js
+++ src/api.js
@@ -1,6 +1,30 @@
+const { google } = require("googleapis");
+
 exports.handler = async function (event, context) {
+    const { sheets, spreadsheetId, auth } = await getSheets();
+
+    // read rows from spreadsheet
+    const getRows = await sheets.spreadsheets.values.get({
+        auth, spreadsheetId,
+        range: "Complaints",
+    });
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello World" }),
+        body: JSON.stringify(getRows.data.values),
     };
+}
+
+const getSheets = async () => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    // create client instance
+    const client = await auth.getClient();
+
+    // create instance of sheets api
+    const sheets = google.sheets({ version: "v4", auth: client });
+
+    const spreadsheetId = "1QKWnUq_fZpdRZAYBNjkNDzyuUCtQqR0s7eDIF7LJ1GA";
+    return { sheets, spreadsheetId, auth };
 }
